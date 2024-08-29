1e550805ca4e66a48b84d3523b2c80ca24e62d5a
Naftali Beder <naftali@naftalibeder.com>
2024-08-29 14:10:06 -0500

    PR - https://github.com/rnmapbox/maps/pull/3602

--- ios/RNMBX/RNMBXCamera.swift
+++ ios/RNMBX/RNMBXCamera.swift
@@ -18,7 +18,7 @@ public enum RemovalReason {
 public protocol RNMBXMapComponent: AnyObject {
   func addToMap(_ map: RNMBXMapView, style: Style)
   func removeFromMap(_ map: RNMBXMapView, reason: RemovalReason) -> Bool
-  
+
   func waitForStyleLoad() -> Bool
 }
 
@@ -34,22 +34,24 @@ struct CameraUpdateItem {
   var camera: CameraOptions
   var mode: CameraMode
   var duration: TimeInterval?
-  
+
   func execute(map: RNMBXMapView, cameraAnimator: inout BasicCameraAnimator?) {
     logged("CameraUpdateItem.execute") {
       if let center = camera.center {
         try center.validate()
       }
 
-      switch mode {
-      case .flight:
-        map.mapView.camera.fly(to: camera, duration: duration)
-      case .ease:
-        map.mapView.camera.ease(to: camera, duration: duration ?? 0, curve: .easeInOut, completion: nil)
-      case .linear:
-        map.mapView.camera.ease(to: camera, duration: duration ?? 0, curve: .linear, completion: nil)
-      default:
-        map.mapboxMap.setCamera(to: camera)
+      map.withMapView { mapView in
+        switch mode {
+        case .flight:
+          mapView.camera.fly(to: camera, duration: duration)
+        case .ease:
+          mapView.camera.ease(to: camera, duration: duration ?? 0, curve: .easeInOut, completion: nil)
+        case .linear:
+          mapView.camera.ease(to: camera, duration: duration ?? 0, curve: .linear, completion: nil)
+        default:
+          mapView.mapboxMap.setCamera(to: camera)
+        }
       }
     }
   }
@@ -57,23 +59,23 @@ struct CameraUpdateItem {
 
 class CameraUpdateQueue {
   var queue: [CameraUpdateItem] = [];
-  
+
   func dequeue() -> CameraUpdateItem? {
     guard !queue.isEmpty else {
       return nil
     }
     return queue.removeFirst()
   }
-  
+
   func enqueue(stop: CameraUpdateItem) {
     queue.append(stop)
   }
-  
+
   func execute(map: RNMBXMapView, cameraAnimator: inout BasicCameraAnimator?) {
     guard let stop = dequeue() else {
       return
     }
-    
+
     stop.execute(map: map, cameraAnimator: &cameraAnimator)
   }
 }
@@ -81,14 +83,16 @@ class CameraUpdateQueue {
 open class RNMBXMapComponentBase : UIView, RNMBXMapComponent {
   private weak var _map: RNMBXMapView! = nil
   private var _mapCallbacks: [(RNMBXMapView) -> Void] = []
-  
+
   weak var map : RNMBXMapView? {
     return _map;
   }
 
   func withMapView(_ callback: @escaping (_ mapView: MapView) -> Void) {
-    withRNMBXMapView { mapView in
-      callback(mapView.mapView)
+    withRNMBXMapView { map in
+      map.withMapView { mapView in
+        callback(mapView)
+      }
     }
   }
 
@@ -99,11 +103,11 @@ open class RNMBXMapComponentBase : UIView, RNMBXMapComponent {
       _mapCallbacks.append(callback)
     }
   }
-  
+
   public func waitForStyleLoad() -> Bool {
     return false
   }
-  
+
   public func addToMap(_ map: RNMBXMapView, style: Style) {
     _mapCallbacks.forEach { callback in
         callback(map)
@@ -111,7 +115,7 @@ open class RNMBXMapComponentBase : UIView, RNMBXMapComponent {
     _mapCallbacks = []
     _map = map
   }
-  
+
   public func removeFromMap(_ map: RNMBXMapView, reason: RemovalReason) -> Bool {
     _mapCallbacks = []
     _map = nil
@@ -123,67 +127,67 @@ open class RNMBXMapComponentBase : UIView, RNMBXMapComponent {
 open class RNMBXCamera : RNMBXMapComponentBase {
   var cameraAnimator: BasicCameraAnimator?
   let cameraUpdateQueue = CameraUpdateQueue()
-  
+
   // MARK: React properties
-  
+
   @objc public var animationDuration: NSNumber?
-  
+
   @objc public var animationMode: NSString?
-  
+
   @objc public var defaultStop: [String: Any]?
-  
+
   @objc public var followUserLocation : Bool = false {
     didSet {
       _updateCameraFromTrackingMode()
     }
   }
-  
+
   @objc public var followUserMode: String? {
     didSet {
       _updateCameraFromTrackingMode()
     }
   }
-  
+
   @objc public var followZoomLevel: NSNumber? {
     didSet {
       _updateCameraFromTrackingMode()
     }
   }
-  
+
   @objc public var followPitch: NSNumber? {
     didSet {
       _updateCameraFromTrackingMode()
     }
   }
-  
+
   @objc public var followHeading: NSNumber? {
     didSet {
       _updateCameraFromTrackingMode()
     }
   }
-  
+
   @objc public var followPadding: NSDictionary? {
     didSet {
       _updateCameraFromTrackingMode()
     }
   }
-  
+
   @objc public var maxZoomLevel: NSNumber? {
     didSet { _updateMaxBounds() }
   }
-  
+
   @objc public var minZoomLevel: NSNumber? {
     didSet { _updateMaxBounds() }
   }
-  
+
   @objc public var onUserTrackingModeChange: RCTBubblingEventBlock? = nil
-  
+
   @objc public var stop: [String: Any]? {
     didSet {
       _updateCamera()
     }
   }
-  
+
   @objc public var maxBounds: String? {
     didSet {
       if let maxBounds = maxBounds {
@@ -197,18 +201,18 @@ open class RNMBXCamera : RNMBXMapComponentBase {
     }
   }
   var maxBoundsFeature : FeatureCollection? = nil
-  
+
   // MARK: Update methods
 
   func _updateCameraFromJavascript() {
     guard !followUserLocation else {
       return
     }
-    
+
     guard let stop = stop else {
       return
     }
-    
+
     /*
     V10 TODO
     if let map = map, map.userTrackingMode != .none {
@@ -232,22 +236,22 @@ open class RNMBXCamera : RNMBXMapComponentBase {
       cameraUpdateQueue.execute(map: map, cameraAnimator: &cameraAnimator)
     }
   }
-  
+
   func _disableUserTracking(_ map: MapView) {
     map.viewport.idle()
   }
-  
+
   @objc public func updateCameraStop(_ stop: [String: Any]) {
     self.stop = stop
   }
-  
+
   func _toCoordinateBounds(_ bounds: FeatureCollection) throws -> CoordinateBounds  {
     guard bounds.features.count == 2 else {
       throw RNMBXError.paramError("Expected two Points in FeatureColletion")
     }
     let swFeature = bounds.features[0]
     let neFeature = bounds.features[1]
-    
+
     guard case let .point(sw) = swFeature.geometry,
           case let .point(ne) = neFeature.geometry else {
       throw RNMBXError.paramError("Expected two Points in FeatureColletion")
@@ -255,11 +259,11 @@ open class RNMBXCamera : RNMBXMapComponentBase {
 
     return CoordinateBounds(southwest: sw.coordinates, northeast: ne.coordinates)
   }
-  
+
   func _updateMaxBounds() {
     withMapView { map in
       var options = CameraBoundsOptions()
-      
+
       if let maxBounds = self.maxBoundsFeature {
         logged("RNMBXCamera._updateMaxBounds._toCoordinateBounds") {
           options.bounds = try self._toCoordinateBounds(maxBounds)
@@ -269,7 +273,7 @@ open class RNMBXCamera : RNMBXMapComponentBase {
       }
       options.minZoom = self.minZoomLevel?.CGFloat
       options.maxZoom = self.maxZoomLevel?.CGFloat
-      
+
       logged("RNMBXCamera._updateMaxBounds") {
         try map.mapboxMap.setCameraBounds(with: options)
       }
@@ -311,20 +315,22 @@ open class RNMBXCamera : RNMBXMapComponentBase {
         followOptions.bearing = nil
         trackingModeChanged = true
       }
-      
+
       if let onUserTrackingModeChange = self.onUserTrackingModeChange {
         if (trackingModeChanged) {
           let event = RNMBXEvent(type: .onUserTrackingModeChange, payload: ["followUserMode": self.followUserMode ?? "normal", "followUserLocation": self.followUserLocation])
           onUserTrackingModeChange(event.toJSON())
         }
       }
-      
+
+      var _camera = CameraOptions()
+
       if let zoom = self.followZoomLevel as? CGFloat {
         if (zoom >= 0.0) {
           followOptions.zoom = zoom
         }
       }
-      
+
       if let followPitch = self.followPitch as? CGFloat {
         if (followPitch >= 0.0) {
           followOptions.pitch = followPitch
@@ -336,9 +342,7 @@ open class RNMBXCamera : RNMBXMapComponentBase {
       } else {
         followOptions.pitch = nil
       }
-      
-      var _camera = CameraOptions()
-      
+
       if let followHeading = self.followHeading as? CGFloat {
         if (followHeading >= 0.0) {
           _camera.bearing = followHeading
@@ -348,7 +352,7 @@ open class RNMBXCamera : RNMBXMapComponentBase {
           _camera.bearing = stopHeading
         }
       }
-      
+
       if let padding = self.followPadding {
         let edgeInsets = UIEdgeInsets(
           top: padding["paddingTop"] as? Double ?? 0,
@@ -358,20 +362,20 @@ open class RNMBXCamera : RNMBXMapComponentBase {
         )
         followOptions.padding = edgeInsets
       }
-      
+
       let followState = map.viewport.makeFollowPuckViewportState(options: followOptions)
-      
+
       map.viewport.transition(to: followState)
       map.viewport.addStatusObserver(self)
       map.mapboxMap.setCamera(to: _camera)
     }
   }
-  
+
   private func toUpdateItem(stop: [String: Any]) -> CameraUpdateItem? {
     if (stop.isEmpty) {
       return nil
     }
-    
+
     var zoom: CGFloat?
     if let z = stop["zoom"] as? Double {
       zoom = CGFloat(z)
@@ -381,28 +385,28 @@ open class RNMBXCamera : RNMBXMapComponentBase {
     if let p = stop["pitch"] as? Double {
       pitch = CGFloat(p)
     }
-    
+
     var heading: CLLocationDirection?
     if let h = stop["heading"] as? Double {
       heading = CLLocationDirection(h)
     }
-    
+
     var padding: UIEdgeInsets = UIEdgeInsets(
       top: stop["paddingTop"] as? Double ?? 0,
       left: stop["paddingLeft"] as? Double ?? 0,
       bottom: stop["paddingBottom"] as? Double ?? 0,
       right: stop["paddingRight"] as? Double ?? 0
     )
-    
+
     var camera: CameraOptions?
-    
+
     if let feature = stop["centerCoordinate"] as? String {
       let centerFeature : Turf.Feature? = logged("RNMBXCamera.toUpdateItem.decode.cc") { try
         JSONDecoder().decode(Turf.Feature.self, from: feature.data(using: .utf8)!)
       }
-      
+
       var center: LocationCoordinate2D?
-      
+
       switch centerFeature?.geometry {
       case .point(let centerPoint):
         center = centerPoint.coordinates
@@ -410,7 +414,7 @@ open class RNMBXCamera : RNMBXMapComponentBase {
         Logger.log(level: .error, message: "RNMBXCamera.toUpdateItem: Unexpected geometry: \(String(describing: centerFeature?.geometry))")
         return nil
       }
-      
+
       camera = CameraOptions(
         center: center,
         padding: padding,
@@ -423,7 +427,7 @@ open class RNMBXCamera : RNMBXMapComponentBase {
       let collection : Turf.FeatureCollection? = logged("RNMBXCamera.toUpdateItem.decode.bound") { try
         JSONDecoder().decode(Turf.FeatureCollection.self, from: feature.data(using: .utf8)!) }
       let features = collection?.features
-      
+
       let ne: CLLocationCoordinate2D
       switch features?.first?.geometry {
         case .point(let point):
@@ -432,7 +436,7 @@ open class RNMBXCamera : RNMBXMapComponentBase {
           Logger.log(level: .error, message: "RNMBXCamera.toUpdateItem: Unexpected geometry: \(String(describing: features?.first?.geometry))")
           return nil
       }
-      
+
       let sw: CLLocationCoordinate2D
       switch features?.last?.geometry {
         case .point(let point):
@@ -441,20 +445,36 @@ open class RNMBXCamera : RNMBXMapComponentBase {
           Logger.log(level: .error, message: "RNMBXCamera.toUpdateItem: Unexpected geometry: \(String(describing: features?.last?.geometry))")
           return nil
       }
-      
+
       withMapView { map in
         #if RNMBX_11
-        let bounds = [sw, ne]
+        do {
+          let bounds: [CLLocationCoordinate2D] = [sw, ne]
+          camera = try map.mapboxMap.camera(
+            for: bounds,
+            camera: .init(cameraState: .init(
+              center: .init(),
+              padding: .zero,
+              zoom: zoom ?? 0,
+              bearing: heading ?? map.mapboxMap.cameraState.bearing,
+              pitch: pitch ?? map.mapboxMap.cameraState.pitch
+            )),
+            coordinatesPadding: padding,
+            maxZoom: nil,
+            offset: nil
+          )
+        } catch {
+          Logger.log(level: .error, message: "RNMBXCamera.toUpdateItem: Failed to build camera configuration: \(error)")
+        }
         #else
         let bounds = CoordinateBounds(southwest: sw, northeast: ne)
-        #endif
-
         camera = map.mapboxMap.camera(
           for: bounds,
           padding: padding,
           bearing: heading ?? map.mapboxMap.cameraState.bearing,
           pitch: pitch ?? map.mapboxMap.cameraState.pitch
         )
+        #endif
       }
     } else {
       camera = CameraOptions(
@@ -475,7 +495,7 @@ open class RNMBXCamera : RNMBXMapComponentBase {
     if let d = stop["duration"] as? Double {
       duration = toSeconds(d)
     }
-    
+
     var mode: CameraMode = .flight
     if let m = stop["mode"] as? String, let m = CameraMode(rawValue: m) {
       mode = m
@@ -487,7 +507,7 @@ open class RNMBXCamera : RNMBXMapComponentBase {
       duration: duration
     )
   }
-  
+
   func _updateCamera() {
     if let _ = map {
       if followUserLocation {
@@ -497,36 +517,37 @@ open class RNMBXCamera : RNMBXMapComponentBase {
       }
     }
   }
-  
+
   func _setInitialCamera() {
     guard let stop = self.defaultStop, let map = map else {
       return
     }
-    
+
     if var updateItem = toUpdateItem(stop: stop) {
       updateItem.mode = .none
       updateItem.duration = 0
       updateItem.execute(map: map, cameraAnimator: &cameraAnimator)
     }
   }
-  
+
   func initialLayout() {
     _setInitialCamera()
     _updateCamera()
   }
-  
+
   public override func addToMap(_ map: RNMBXMapView, style: Style) {
     super.addToMap(map, style: style)
     map.reactCamera = self
   }
-  
+
   public override func removeFromMap(_ map: RNMBXMapView, reason: RemovalReason) -> Bool {
     if (reason == .StyleChange) {
       return false
     }
 
-    map.mapView.viewport.removeStatusObserver(self)
-    return super.removeFromMap(map, reason:reason)
+    map._mapView.viewport.removeStatusObserver(self)
+
+    return super.removeFromMap(map, reason: reason)
   }
 }
 
@@ -565,12 +586,12 @@ extension RNMBXCamera : ViewportStatusObserver {
         return "compass"
       case .course:
         return "course"
-      case .some(let bearing):
+      case .some(_):
         return "constant"
       case .none:
         return "normal"
       }
-    } else if let state = state as? OverviewViewportState {
+    } else if let _ = state as? OverviewViewportState {
       return "overview"
     } else {
       return "custom"
